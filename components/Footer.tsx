5858d8888be213dc93a7eee1466af1a0dbeb3e88
Shuyong Yang <shuyong.yang@daolabs.com>
2025-04-05 06:30:36 +0400

    change footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,8 +3,8 @@ export default function Footer() {
     <footer className="w-full border-t border-red-800 mt-20 py-6 text-xs text-gray-400 font-mono flex flex-col sm:flex-row justify-between items-center px-4 gap-2">
       <div>&copy; {new Date().getFullYear()} New SALT Corp</div>
       <div className="flex flex-wrap gap-4 text-red-400">
-        <a href="https://twitter.com/NewSALTCorp" target="_blank" rel="noopener noreferrer">Twitter</a>
-        <a href="https://t.me/newsaltcorp" target="_blank" rel="noopener noreferrer">Telegram</a>
+        <a href="https://twitter.com/SALT_Corp" target="_blank" rel="noopener noreferrer">Twitter</a>
+        <a href="https://t.me/salt_corp" target="_blank" rel="noopener noreferrer">Telegram</a>
         <a href="https://github.com/yourrepo" target="_blank" rel="noopener noreferrer">GitHub</a>
         <a href="https://pump.fun/coin/" target="_blank" rel="noopener noreferrer">Pump.fun</a>
         <a href="https://dexscreener.com/solana/yourpair" target="_blank" rel="noopener noreferrer">Dexscreener</a>
