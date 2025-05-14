ae4a8887bbcc3df83e98234eb30e23264eadd39c
Shoyu <shuyong.yang@daolabs.com>
2025-05-15 03:14:17 +0700

    fix links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,9 +4,7 @@ export default function Footer() {
       <div>&copy; {new Date().getFullYear()} New SALT Corp</div>
       <div className="flex flex-wrap gap-4 text-red-400">
         <a href="https://twitter.com/SALT_Corp" target="_blank" rel="noopener noreferrer">Twitter</a>
-        <a href="https://t.me/salt_corp" target="_blank" rel="noopener noreferrer">Telegram</a>
         <a href="https://github.com/yourrepo" target="_blank" rel="noopener noreferrer">GitHub</a>
-        <a href="https://pump.fun/coin/" target="_blank" rel="noopener noreferrer">Pump.fun</a>
         <a href="https://dexscreener.com/solana/yourpair" target="_blank" rel="noopener noreferrer">Dexscreener</a>
       </div>
     </footer>
